7f5b283f6fcb610c915aea38032c4c7dd357cbff
synth9283 <synth9283@gmail.com>
2020-08-08 23:00:54 +1000

    Fixed mainMenu.c rendering

--- src/mainMenu.c
+++ src/mainMenu.c
@@ -3,9 +3,9 @@
 #include "../headers/objects.h"
 
 void mainMenu(SDL_Renderer *renderer, SDL_Surface *surface, Game *game) {
-  surface = IMG_Load("../img/space.png");
+  surface = IMG_Load("img/space.png");
   game->spaceTexture = SDL_CreateTextureFromSurface(renderer, surface);
-  SDL_Rect rect = {game->w/2, game->h/2, game->w, game->h};
+  SDL_Rect rect = {0, 0, game->w, game->h};
   SDL_RenderCopyEx(renderer, game->spaceTexture, NULL, &rect, 0, NULL, 0);
   SDL_RenderPresent(renderer);
 
