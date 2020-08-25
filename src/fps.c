80afc6c02695264aed9a449c369b90d63f92366c
synth9283 <synth9283@gmail.com>
2020-08-25 15:50:48 +1000

    Update fps.c

--- src/fps.c
+++ src/fps.c
@@ -2,10 +2,11 @@
 #include <SDL2/SDL_timer.h>
 
 #define FPS 60
+#define FRAME_DELAY 1000/FPS
 
 void fps(Uint32 ticks) {
     Uint32 frame = SDL_GetTicks() - ticks;
-    if ((1000/FPS) > frame) {
-        SDL_Delay((1000/FPS) - frame);
+    if (FRAME_DELAY > frame) {
+        SDL_Delay(FRAME_DELAY-frame);
     }
 }
